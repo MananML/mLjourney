0bc0afbb7f46bca5c0300e30ffe5577cc93e3d38
MananML <218400437+MananML@users.noreply.github.com>
2026-03-22 16:36:24 +0100

    first commit

--- cs50/practice/slot_machine.py
+++ cs50/practice/slot_machine.py
@@ -116,11 +116,3 @@ def bet():
 
 
 
-
-echo "# python_journey" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin 
-git push -u origin main
