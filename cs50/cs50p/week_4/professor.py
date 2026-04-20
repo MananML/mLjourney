3ccd607d1494446cf12aa283a99e1d8792e2060c
MananML <218400437+MananML@users.noreply.github.com>
2026-04-20 22:16:03 +0100

    updated professor.py

--- cs50/cs50p/week_4/professor.py
+++ cs50/cs50p/week_4/professor.py
@@ -1,9 +1,24 @@
 import random
 
-levels = [1, 2, 3]
+levels = ["1", "2", "3"]
 
 def main():
-    ...
+    level = get_level()
+    for i in range(11):
+        x, y = generate_integer(level)
+
+        question = input(f"{x} + {y} = ")
+        answer = eval(question)
+
+        if question == answer:
+            continue
+            
+        for _ in range(3):
+            if question == answer:
+                break
+        else:
+            print(answer)  
+            
 
 
 def get_level():
@@ -15,8 +30,11 @@ def get_level():
             return level
 
 def generate_integer(level):
-    ...
+    x = random.randint(0, pow(10, level))
+    y = random.randint(0, pow(10, level))    
 
+    pair = x, y
+    return pair
 
 if __name__ == "__main__":
     main()
